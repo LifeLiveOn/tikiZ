28dffcb885bb7fab692779699441b70f84234cea
LifeLiveOn <86644469+LifeLiveOn@users.noreply.github.com>
2021-07-21 09:18:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -314,7 +314,7 @@ window.onload = function() {
   
             var cmessage_content = document.createElement('p')
             cmessage_content.setAttribute('class', 'cmessage_content')
-            cmessage_content.textContent = `${message}`
+            cmessage_content.textContent = `${message} ${Date.now()}`
               
             message_user_container.append(cmessage_user)
             message_content_container.append(cmessage_content)
@@ -327,7 +327,7 @@ window.onload = function() {
   
             var message_content = document.createElement('p')
             message_content.setAttribute('class', 'message_content')
-            message_content.textContent = `${message}`
+            message_content.textContent = `${message} ${Date.now()}`
                 
             message_user_container.append(message_user)
             message_content_container.append(message_content)
